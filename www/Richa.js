a5db12598d175f091e9bc409ed829fcfb84f3434
richa <richa@dyfolabs.com>
2023-02-19 21:37:14 +0530

    modified js

--- www/Richa.js
+++ www/Richa.js
@@ -1,5 +1,4 @@
-cordova.define("cordova-plugin-richa.Richa", function(require, exports, module) {
-    var exec = require('cordova/exec');
+var exec = require('cordova/exec');
     
 var Richa={};
 Richa.start=function(arg0, success, error){
@@ -7,4 +6,3 @@ Richa.start=function(arg0, success, error){
 }
 
 module.exports=Richa
-})
